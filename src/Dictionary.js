8d695aa95bbd4f214465effd5c0971ce740a6f3f
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-06-12 13:58:50 +0100

    Installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setkeyword(event.target.value);
