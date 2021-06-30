a99d284be6f069ac5994fb8f743fbe70007bafed
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-06-30 10:53:50 +0100

    Integrated and displayed the photos api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setkeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setphotos] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setphotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    //documentation:https://www.pexels.com/api/documentation/#photos-search
+    let pexelsApiKey =
+      "563492ad6f91700001000001037806324dcf447e9ed736865a540f65";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
+
   function handleKeywordChange(event) {
     setkeyword(event.target.value);
   }
@@ -39,12 +54,13 @@ export default function Dictionary(props) {
               className="form-input"
               type="search"
               onChange={handleKeywordChange}
-              defaultKeyword={props.defaultkeyword}
+              defaultValue={props.defaultKeyword}
             />
           </form>
         </section>
         <section>
           <Results results={results} />
+          <Photos photos={photos} keyword={keyword} />
         </section>
       </div>
     );
